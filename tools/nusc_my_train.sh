bd2c3794cdfe8ff8c52a1c6bc7df325859f9c5cf
ucaszyp <zhengyupeng18@mails.ucas.ac.cn>
2025-01-29 17:00:27 +0800

    init

--- tools/nusc_my_train.sh
+++ tools/nusc_my_train.sh
@@ -7,7 +7,7 @@ PORT=${PORT:-59230}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     train.py $CONFIG \
-    --work-dir work_dirs/train1 \
+    --work-dir work_dirs/train_swin_wopre \
     --launcher pytorch ${@:3} \
     --deterministic \
     --cfg-options evaluation.jsonfile_prefix=work_dirs/$1/eval/results 
